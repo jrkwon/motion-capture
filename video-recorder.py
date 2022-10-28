6b7208b746601e81285beb714096e28a4a7e25d9
Jaerock Kwon <jaerock.kwon@gmail.com>
2022-10-28 17:01:47 -0400

    change help msg

--- video-recorder.py
+++ video-recorder.py
@@ -18,8 +18,8 @@ class VideoRecoder():
     RES_480P  = 0
     RES_720P  = 1
     RES_1080P = 2
-    
-    
+
+
     def __init__(self, target_dir, camera_id, time_zone):
         self.image_ext = '.jpg'
         self.camera_id = camera_id
@@ -55,7 +55,7 @@ class VideoRecoder():
         elif res_id == self.RES_480P:
             self._make_480p()
 
-    
+
     def get_time_stamp(self):
         unix_time = time.time()
         if self.time_zone == "utc":
@@ -63,7 +63,7 @@ class VideoRecoder():
         else:
             time_stamp = datetime.datetime.fromtimestamp(unix_time).strftime("%Y-%m-%d-%H-%M-%S-%f")
         return "{:.6f}={}".format(unix_time, time_stamp)
-    
+
 
     def create_directory(self, target_dir):
         directory = self.get_time_stamp()
@@ -71,7 +71,7 @@ class VideoRecoder():
         os.makedirs(path)
         print("Directory {} created.".format(path))
         return path
-    
+
 
 def main(resolution, verbose, target_dir, camera_id, time_zone):
     # define a video capture object
@@ -81,29 +81,29 @@ def main(resolution, verbose, target_dir, camera_id, time_zone):
 
     path = vr.create_directory(target_dir)
     vr.set_resolution(resolution)
-        
+
     print("Start recording... Press 'q' to stop.")
     while(True):
-          
+
         # Capture the video frame
         # by frame
         ret, frame = vr.video.read()
-      
+
         # Display the resulting frame
         filename = vr.get_time_stamp() + vr.image_ext
-        
+
         cv2.imshow('Camera ' + str(camera_id), frame)
         file_path = os.path.join(path, filename)
         if verbose:
             print(file_path)
         cv2.imwrite(file_path, frame)
-          
+
         # the 'q' button is set as the
         # quitting button you may use any
         # desired button of your choice
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-      
+
     # After the loop release the cap object
     vr.video.release()
     # Destroy all the windows
@@ -113,16 +113,16 @@ def main(resolution, verbose, target_dir, camera_id, time_zone):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Video Recorder ver 0.3 by Jaerock Kwon, 2021')
-    parser.add_argument("-r", "--resolution", type=int, default=0, choices=(0, 1, 2), 
-                    help="resolution id number: 0:640x480, 1:1280x720, 2:1920x1080")
+    parser.add_argument("-r", "--resolution", type=int, default=0, choices=(0, 1, 2),
+                    help="resolution id: 0:640x480, 1:1280x720, 2:1920x1080")
     parser.add_argument("-v", "--verbose", action="store_true",
                     help="print filenames")
-    parser.add_argument("-c", "--camera_id", type=int, default=0, choices=(0, 1, 2, 3), 
+    parser.add_argument("-c", "--camera_id", type=int, default=0,
                         help="camera id number")
-    parser.add_argument("-t", "--time_zone", default="utc", 
-                        choices=("utc", "local"), 
+    parser.add_argument("-t", "--time_zone", default="utc",
+                        choices=("utc", "local"),
                         help="time zone: default utc")
     parser.add_argument("target_dir", type=str, help="target directory name")
     args = parser.parse_args()
-    
+
     main(args.resolution, args.verbose, args.target_dir, args.camera_id, args.time_zone)
